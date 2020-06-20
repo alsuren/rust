cc572eb230247b22b7b199fd5a31c22947c490e3
David Laban <alsuren@gmail.com>
2020-06-20 18:50:42 +0000

    simplify harder

--- src/test/ui/impl-trait/where-allowed.rs
+++ src/test/ui/impl-trait/where-allowed.rs
@@ -1,15 +1,2 @@
-//! A simple test for testing many permutations of allowedness of
-//! impl Trait
 use std::fmt::Debug;
-
-// Allowed
-fn in_parameters(_: impl Debug) { panic!() }
-
-// Allowed
-fn in_return() -> impl Debug { panic!() }
-
-// Allowed
-fn in_adt_in_parameters(_: Vec<impl Debug>) { panic!() }
-
-// Allowed
 fn in_impl_Fn_return_in_parameters(_: &impl Fn() -> impl Debug) { panic!() }
